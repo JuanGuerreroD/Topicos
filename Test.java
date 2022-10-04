23c5d7bb4e372e0397461ad62da9ea0320e5bea0
JuanGuerreroD <112977258+JuanGuerreroD@users.noreply.github.com>
2022-10-04 12:29:30 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package LogFrame;
+
+import javax.swing.JFrame;
+
+public class Test {
+
+    
+    public static void main(String[] args) {
+        
+        LoginFrameLayout frame = new LoginFrameLayout();
+        frame.setSize(400, 250);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   }
+    
+}
